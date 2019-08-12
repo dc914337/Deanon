25a6cd6fea391db5ae9ff4556a14ae95edc9663c
Konstantin Safonov <kasthack@epicm.org>
2019-08-12 21:24:24 +0300

    Switch to json-based configuration from interactive input

---
+++ Deanon/Deanon/Config.cs
@@ -0,0 +1,42 @@
+﻿namespace Deanon.Configuration
+{
+    class Config
+    {
+        public Vk VK { get; set; }
+        public Db DB { get; set; }
+        public Depth Depth { get; set; }
+        public Stages Stages { get; set; }
+        public Exec Exec { get; set; }
+    }
+    public class Exec
+    {
+        public int UserId { get; set; }
+        public bool ContinueFromSavePoint { get; set; }
+        public bool CompleteRelations { get; set; }
+        public int Expansions { get; set; }
+    }
+    public class Stages
+    {
+        public bool Small { get; set; }
+        public bool Big { get; set; }
+    }
+    public class Depth
+    {
+        public int Friends { get; set; }
+        public int Followers { get; set; }
+        public int Post { get; set; }
+        public int Comments { get; set; }
+        public int Likes { get; set; }
+    }
+    class Vk
+    {
+        public string Token { get; set; }
+    }
+    public class Db
+    {
+        public string Address { get; set; }
+        public int Port { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+    }
+}
