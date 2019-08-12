a898693d418423ca178bee8bf139d9ba95b2c67c
Konstantin Safonov <kasthack@epicm.org>
2019-08-12 22:06:18 +0300

    Formatting

--- Deanon/Deanon/DB/DataModels/Relationships/RelationData/InteractionData.cs
+++ Deanon/Deanon/DB/DataModels/Relationships/RelationData/InteractionData.cs
@@ -1,11 +1,11 @@
 ﻿namespace Deanon.db.datamodels.classes.relationships.RelationData
 {
     public class InteractionData
-	{
+    {
         public InteractionType interactionType { get; }
 
         public string TypeName => this.interactionType.ToString();
-		public int Weight => (int)this.interactionType;
+        public int Weight => (int)this.interactionType;
 
         public InteractionData(InteractionType type) => this.interactionType = type;
     }
--- Deanon/Deanon/DB/DataModels/Relationships/RelationData/InteractionType.cs
+++ Deanon/Deanon/DB/DataModels/Relationships/RelationData/InteractionType.cs
@@ -1,11 +1,11 @@
 ﻿namespace Deanon.db.datamodels.classes.relationships.RelationData
 {
-	public enum InteractionType
-	{
-		Frientd = 1,
-		Posted = 2,
-		LeftComment = 3,
-		LikedPost = 4,
-		LikedComment = 5
-	};
+    public enum InteractionType
+    {
+        Frientd = 1,
+        Posted = 2,
+        LeftComment = 3,
+        LikedPost = 4,
+        LikedComment = 5
+    };
 }
